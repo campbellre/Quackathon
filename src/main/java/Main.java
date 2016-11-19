59e28e6af0514fc273e8a94f40d15d34fa167146
campbellre <campbellre92@outlook.com>
2016-11-19 15:28:24 +0000

    Initial Commit, Hello World!

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ryan on 19/11/2016.
+ */
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
